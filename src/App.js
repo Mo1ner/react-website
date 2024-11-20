cad6984b88fbac52914bd19ec2a0850cd5232f7a
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-20 21:53:43 +0100

    about-section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Categories from "./components/categories/Categories";
 import Collection from "./components/collection/Collection";
+import About from "./components/about/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <Categories />
       <Collection />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import aboutImg from "./../../img/main/Mask group.png";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="about-row">
+          <div className="about-img">
+            <img src={aboutImg} alt="img" />
+          </div>
+          <div className="about-wrapper">
+            <div className="about-title">
+              <h2>About us</h2>
+            </div>
+            <div className="about-text">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of
+                Lorem Ipsum.
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+            <div className="about-btn">
+              <a href="!#">Learn more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
