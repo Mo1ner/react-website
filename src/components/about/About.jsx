b2443a3fc25c5bcaa6ab76e09f44e8ca31599f37
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-21 16:24:01 +0100

    about-jsx-update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,7 +26,8 @@ const About = () => {
                 containing Lorem Ipsum passages, and more recently with desktop
                 publishing software like Aldus PageMaker including versions of
                 Lorem Ipsum.
-                <br />
+              </p>
+              <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
