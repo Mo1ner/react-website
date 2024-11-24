86e6f7c6d32c5587000741f42177486b9b2757e5
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-24 15:50:14 +0100

    navbar-adaptive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+import burgerMenu from "./../../img/header/menu.png";
+
 import "./navbar.css";
 
-const navbar = () => {
+const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const togglemenu = () => {
+    setMobileMenu(!mobileMenu);
+    console.log(mobileMenu);
+  };
+
   return (
     <div className="header">
       <div className="container">
         <div className="header-row">
           <div className="header-logo">
-            <a href="!#">ALTANSCHOOL web COURSES</a>
+            <a href="!#">ALTANSCHOOL WEB COURSES</a>
           </div>
           <nav className="nav">
-            <ul>
+            <ul className={`${mobileMenu ? "open-mobile-menu" : ""}`}>
               <li>
                 <a href="!#">HOME</a>
               </li>
@@ -27,6 +37,14 @@ const navbar = () => {
                 <a href="!#">CONTACT</a>
               </li>
             </ul>
+            <img
+              src={burgerMenu}
+              onClick={togglemenu}
+              className={`${
+                mobileMenu ? "mobile-menu mobile-menu-fixed" : "mobile-menu"
+              }`}
+              alt="mobile menu"
+            />
           </nav>
         </div>
       </div>
@@ -34,4 +52,4 @@ const navbar = () => {
   );
 };
 
-export default navbar;
+export default Navbar;
